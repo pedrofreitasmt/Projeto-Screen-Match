53b667aa51b0aef87ae9a8486e0d2566ce83d9bd
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-06-13 11:52:56 -0400

    Segundo dia codando em php. Aprendendo a manipular arrays.

--- pagina-inicial.php
+++ pagina-inicial.php
@@ -1,13 +1,40 @@
 <?php
 
-$nomeFilme = 'Top Gun - Maverick';
+$nomeFilme = "Top Gun - Maverick";
+
 $anoLancamento = 2022;
-$somaDeNotas = (9 + 6 + 8 + 7.5 + 5);
-$notaFilme = $somaDeNotas / 5;
+$quantidadeDeNotas = $argc - 1;
+$notas = [];
+
+for ($contador = 1; $contador < $argc; $contador++) {
+    $notas[] = (float) $argv[$contador];
+}
+
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
 $planoPrime = true;
 
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
 echo "Bem-vindo(a) ao screen match!\n";
-echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nome do filme: $nomeFilme\n";
 echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento.\n";
+} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo "Esse filme ainda é novo\n";
+} else {
+    echo "Esse filme não é um lançamento.\n";
+}
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok"=> "super-herói",
+    "Se beber não case"=> "comédia",
+    default => "Gênero não encontrado.",
+};
+
+echo "O gênero do filme é: $genero\n";
+
+$filme = ["nome" => "Thor: Ragnarok", "ano" => 2021, "nota" => 7.8, "genero" => "super-herói"];
