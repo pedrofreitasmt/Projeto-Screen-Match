520509769814eb9448d76fa9bdf3ba506e8080be
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-06-27 12:03:18 -0400

    "funcoes-screen-match" foi comentado temporariamente

--- src/funcoes-screen-match.php
+++ src/funcoes-screen-match.php
@@ -1,27 +1,27 @@
 <?php
 
-function exibeMensagemLancamento(int $ano): void {
-    if ($ano > 2022) {
-        echo "Esse filme é um lançamento.\n";
-    } elseif ($ano > 2020 && $ano <= 2022) {
-        echo "Esse filme ainda é novo\n";
-    } else {
-        echo "Esse filme não é um lançamento.\n";
-    }
-}
+// function exibeMensagemLancamento(int $ano): void {
+//     if ($ano > 2022) {
+//         echo "Esse filme é um lançamento.\n";
+//     } elseif ($ano > 2020 && $ano <= 2022) {
+//         echo "Esse filme ainda é novo\n";
+//     } else {
+//         echo "Esse filme não é um lançamento.\n";
+//     }
+// }
 
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
-    return $planoPrime || $anoLancamento < 2020;
-}
+// function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
+//     return $planoPrime || $anoLancamento < 2020;
+// }
 
-function criaFilme(string $nome, int $anoLancamento, float $notas, string $genero): Filme
-{
-    $filme = new Filme();
+// function criaFilme(string $nome, int $anoLancamento, float $notas, string $genero): Filme
+// {
+//     $filme = new Filme();
 
-    $filme->nome = $nome;
-    $filme->anoLancamento = $anoLancamento;
-    $filme->genero = $genero;
-    $filme->notas = $notas;
+//     $filme->nome = $nome;
+//     $filme->anoLancamento = $anoLancamento;
+//     $filme->genero = $genero;
+//     $filme->notas = $notas;
 
-    return $filme;
-};
+//     return $filme;
+// };
