5e03c4aac6f8f3735d02c25c7c08f46357356996
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-01 09:05:36 -0400

    Registrando uma serie para teste e importando a classe "Serie" para index.php

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
+require __DIR__ . "/src/Modelo/Titulo.php";
 require __DIR__ . "/src/Modelo/Genero.php";
+require __DIR__ . "/src/Modelo/Serie.php";
 require __DIR__ . "/src/Modelo/Filme.php";
 
 echo "Bem-vindo(a) ao Screen-Match!\n";
 
-$filme = new Filme("Vingadores", 2012, Genero::SuperHeroi);
+$filme = new Filme("Vingadores", 2012, Genero::SuperHeroi, 180);
 
 $filme->avalia(10);
 $filme->avalia(7);
 $filme->avalia(8.2);
 $filme->avalia(6.7);
 
-echo $filme->nome;
+$breakingBad = new Serie("Breaking Bad", 2008, Genero::Drama, 13, 5, 70);
+
+print_r($breakingBad->anoLancamento);
