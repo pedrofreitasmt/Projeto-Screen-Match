8454ac247c19d340ea846f917a766af48d6b0ee4
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-03 11:59:11 -0400

    Classe episódio criada para avaliar os episódios das séries

---
+++ src/Modelo/Episodio.php
@@ -0,0 +1,27 @@
+<?php
+
+class Episodio implements Avaliavel
+{
+    private array $notas;
+
+    public function __construct(
+        public readonly Serie $serie,
+        public readonly string $nome,
+        public readonly int $numero,
+    ){
+        $this->notas = [];
+    }
+
+    public function avalia(float $nota): void
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeDeNotas = count($this->notas);                                                                                               
+        return $somaNotas / $quantidadeDeNotas;
+    }
+    
+}
