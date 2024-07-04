796e686c8ec1fd7bba02176d49ef3309acce9235
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-04 10:53:25 -0400

    Criado o namespace principal "ScreenMatch" e os arquivos ficaram mais resumidos com o uso da trait "ComAvaliacao"

--- src/Modelo/Titulo.php
+++ src/Modelo/Titulo.php
@@ -1,31 +1,16 @@
 <?php
 
+namespace ScreenMatch\Modelo;
+
 abstract class Titulo implements Avaliavel
 {
-    private array $notas;
+    use ComAvaliacao;
+
     public function __construct(
         public readonly string $nome,
         public readonly int $anoLancamento,
         public readonly Genero $genero,
-    ) {
-        $this->notas = [];
-    }
-
-    public function avalia(float $nota): void
-    {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float
-    {
-        $quantidadeDeNotas = count($this->notas);
-        if ($quantidadeDeNotas === 0) {
-            return 0; // Retorne 0 ou um valor padrão
-        }
-
-        $somaNotas = array_sum($this->notas);
-        return $somaNotas / $quantidadeDeNotas;
-    }
+    ) {}
 
     abstract public function duracaoEmMinutos(): int;
 }
