f80f40e0c2258a0100944475a982471b7e1fd0d0
PedroFreitas <pedrohenrique_of@hotmail.com>
2024-07-04 10:48:27 -0400

    "antigo.php" foi comentado temporariamente

--- antigo.php
+++ antigo.php
@@ -1,42 +1,42 @@
 <?php
-require __DIR__ . "/src/Modelo/Filme.php";
-require __DIR__ . "/src/funcoes-screen-match.php";
+// require __DIR__ . "/src/Modelo/Filme.php";
+// require __DIR__ . "/src/funcoes-screen-match.php";
 
-$nomeFilme = "Top Gun - Maverick";
+// $nomeFilme = "Top Gun - Maverick";
 
-$anoLancamento = 2022;
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
+// $anoLancamento = 2022;
+// $quantidadeDeNotas = $argc - 1;
+// $notas = [];
 
-for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $argv[$contador];
-}
+// for ($contador = 1; $contador < $argc; $contador++) {
+//     $notas[] = (float) $argv[$contador];
+// }
 
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-$planoPrime = true;
+// $notaFilme = array_sum($notas) / $quantidadeDeNotas;
+// $planoPrime = true;
 
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
+// $incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
 
-echo "Bem-vindo(a) ao screen match!\n";
-echo "Nome do filme: $nomeFilme\n";
-echo "Nota do filme: $notaFilme\n";
-echo "Ano de lançamento: $anoLancamento\n";
+// echo "Bem-vindo(a) ao screen match!\n";
+// echo "Nome do filme: $nomeFilme\n";
+// echo "Nota do filme: $notaFilme\n";
+// echo "Ano de lançamento: $anoLancamento\n";
 
-$genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação",
-    "Thor: Ragnarok"=> "super-herói",
-    "Se beber não case"=> "comédia",
-    default => "Gênero não encontrado.",
-};
+// $genero = match ($nomeFilme) {
+//     "Top Gun - Maverick" => "ação",
+//     "Thor: Ragnarok"=> "super-herói",
+//     "Se beber não case"=> "comédia",
+//     default => "Gênero não encontrado.",
+// };
 
-echo "O gênero do filme é: $genero\n";
+// echo "O gênero do filme é: $genero\n";
 
-$filme = criaFilme(nome:"Thor: Ragnarok", anoLancamento: 2021, nota: 7.8, genero:"super-herói");
+// $filme = criaFilme(nome:"Thor: Ragnarok", anoLancamento: 2021, nota: 7.8, genero:"super-herói");
 
-echo $filme -> anoLancamento;
+// echo $filme -> anoLancamento;
 
-$posicaoDoisPontos = strpos($filme -> nome, ':');
+// $posicaoDoisPontos = strpos($filme -> nome, ':');
 
-$filmeComoStringJson = json_encode($filme);
+// $filmeComoStringJson = json_encode($filme);
 
-file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
+// file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
